edf18c99e241ecfbc20bc4e5bfe9cb9b7d452847
jayan <jkandathil@stratuscent.com>
2023-06-25 02:14:04 -0400

    Updates

---
+++ precache-manifest.4a6d1dcd3fee9a118044d5e34a02e66d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d85cd296630487bf59dd39f1e475bbc",
+    "url": "/diaganoze/index.html"
+  },
+  {
+    "revision": "801bc06602b4b24b5379",
+    "url": "/diaganoze/static/css/main.b9d07cc3.chunk.css"
+  },
+  {
+    "revision": "f9891c60f3d47f2936ec",
+    "url": "/diaganoze/static/js/2.1289e2a4.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/diaganoze/static/js/2.1289e2a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "801bc06602b4b24b5379",
+    "url": "/diaganoze/static/js/main.944b3639.chunk.js"
+  },
+  {
+    "revision": "723a5bb8af2ea85e5952",
+    "url": "/diaganoze/static/js/runtime-main.046b7090.js"
+  }
+]);
